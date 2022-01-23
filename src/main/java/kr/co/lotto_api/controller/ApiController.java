486722b09fc6353184b2c1d22e91db03e636e009
kjm4311 <kjm4311@naver.com>
2022-01-24 01:08:27 +0900

    add insert api ( post )

--- src/main/java/kr/co/lotto_api/controller/ApiController.java
+++ src/main/java/kr/co/lotto_api/controller/ApiController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import kr.co.lotto_api.model.WinNoVO;
 import kr.co.lotto_api.service.WinNoService;
  
@@ -22,12 +25,27 @@ public class ApiController {
 	@Autowired
     WinNoService winNoService;
    
+	ObjectMapper  om = new ObjectMapper();
+	
     @RequestMapping(value = "/winNo", method = RequestMethod.GET)
-    public @ResponseBody String get(@RequestParam Map<String,Object> map) {
+    public @ResponseBody String get(@RequestParam Map<String,Object> map) throws JsonProcessingException {
     	List<WinNoVO> list = winNoService.selectList(map);
+    	
+        return om.writeValueAsString(list);
+    }
+    
+    @RequestMapping(value = "/winNo", method = RequestMethod.POST)
+    public @ResponseBody String set(@RequestParam Map<String,Object> map) throws JsonProcessingException {
     	JSONObject json = new JSONObject();
-    	json.put("list", list);
-        return json.toJSONString();
+    	try {
+    		winNoService.insert(map);
+    		json.put("status", "OK");
+    	}catch(Exception e) {
+    		json.put("status", "Exception");
+    	}
+    	return json.toJSONString();
     }
  
+    
+    
 }
