77a862f67aa1161537f3453705258f31307bc414
kjm4311 <kjm4311@naver.com>
2022-01-30 01:59:23 +0900

    winNO

--- src/main/java/kr/co/lotto_api/model/WinNoVO.java
+++ src/main/java/kr/co/lotto_api/model/WinNoVO.java
@@ -1,5 +1,9 @@
 package kr.co.lotto_api.model;
 
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +13,17 @@ public class WinNoVO{
 	
 	private int drwNo; //회차
 	private String dt; //추첨일
+	@JsonIgnore
 	private int no1,no2,no3,no4,no5,no6,no7;
+	private int[] nos ;
 	
+	public void setNo7(int no7) {
+		this.no7 = no7;
+		this.nos = new int[]{no1,no2,no3,no4,no5,no6,no7};
+	}
+
+	@Override
+	public String toString() {
+		return "{drwNo:" + drwNo + ", dt:" + dt + ", nos:" + Arrays.toString(nos) + "}";
+	}
 }
