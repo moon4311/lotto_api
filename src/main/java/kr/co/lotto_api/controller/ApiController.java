b6b111b2378119bac5cbe13cbeb804b71b572497
kjm4311 <kjm4311@naver.com>
2022-02-27 23:59:51 +0900

    결과입력 로직 구현

--- src/main/java/kr/co/lotto_api/controller/ApiController.java
+++ src/main/java/kr/co/lotto_api/controller/ApiController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -42,7 +43,7 @@ public class ApiController {
     }
     
     @PostMapping(value = "/winNo")
-    public @ResponseBody String set(@RequestParam Map<String,Object> map) throws JsonProcessingException {
+    public @ResponseBody String set(@RequestBody Map<String,Object> map) throws JsonProcessingException {
     	JSONObject json = new JSONObject();
     	try {
     		winNoService.insert(map);
