141ef4faad9d02093ed8c7cfc55427d23dc9da63
kjm4311 <kjm4311@naver.com>
2022-05-10 01:46:41 +0900

    ball_no_tb -> ball_no_rate_tb

--- src/main/java/kr/co/lotto_api/mapper/WeekBatch.java
+++ src/main/java/kr/co/lotto_api/mapper/WeekBatch.java
@@ -13,10 +13,17 @@ public interface WeekBatch {
 
 	// 814.. pastYn
 	public int updateLottoNo();
-	// 각 자리별 숫자 출현 수
+	
+	// 각 자리별 숫자 출현 수 v1
+	@Deprecated
 	public int updateBallNo();
+	
+	// 각 자리별 숫자 출현 수 v2
+	public int updateBallNoRate();
+	
 	// 합계 비율
 	public int updateSumNo();
+	
 	// 홀짝 비율
 	public int updateOddNo();
 
